cc1905824a5a69b3f193b7dca83e355efd9cd270
aya-cs22 <aya786930@gmail.com>
2023-12-17 09:28:30 -0800

    monty

---
+++ fun2.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * _pint - prints the stack
+ * @head: head of the stack
+ * @line: line number
+ * Return: nothing
+ */
+void _pint(stack_t **head, unsigned int line)
+{
+	if (temp == NULL)
+	{
+		return;
+	}
+	printf("%d\n", temp->n);
+}
