2514d68bd1a9766aacc69b33bb85b004691183ac
aya-cs22 <aya786930@gmail.com>
2023-12-17 09:17:20 -0800

    monty

--- func.c
+++ func.c
@@ -7,12 +7,10 @@
  */
 void _pint(stack_t **head, unsigned int line)
 {
-	stack_t *temp = NULL;
-	(void)line;
-	temp = *head;
 	if (temp == NULL)
 	{
-		return;
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", temp->n);
 }
